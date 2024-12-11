fe3d13f7f02f7870524bad57606329a32e03c0dc
Igor Cotrim Santos <igorcotrim.dev@gmail.com>
2024-12-11 20:09:47 -0300

    create Home scene

---
+++ NearbyApp/Sources/Features/Models/Place.swift
@@ -0,0 +1,21 @@
+//
+//  Place.swift
+//  NearbyApp
+//
+//  Created by Igor Cotrim on 10/12/24.
+//
+
+import Foundation
+
+struct Place: Decodable {
+    let id: String
+    let name: String
+    let description: String
+    let coupons: Int
+    let latitude: Double
+    let longitude: Double
+    let address: String
+    let phone: String
+    let cover: String
+    let categoryId: String
+}
